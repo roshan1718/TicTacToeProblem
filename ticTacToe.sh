41d3361339ffd3ffa43e5e7835ba432e67737ca5
roshan1718 <roshanshinde1717@gmail.com>
2020-03-23 13:59:54 +0530

    ADDED code to check who plays first using toss

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -43,18 +43,40 @@ displayBoard()
 
 
 letterAssign(){
-	if [[ $((RANDOM%2)) -eq 0 ]];	then
+	if [[ $((RANDOM%2)) -eq 0 ]];	
+	then
 		playerOne=X
 		PlayerTwo=O
 		echo "Player1 ==> X"
 		echo "Player2 ==> O"
 	else
 		playerOne=O
-      PlayerTwo=X
-      echo "Player1 ==> O"
-      echo "Player2 ==> X"
+		PlayerTwo=X
+		echo "Player1 ==> O"
+		echo "Player2 ==> X"
 	fi
 }
+
+toss(){
+	if [[ $((RANDOM%2)) -eq 0 ]]; then
+		echo "Player1 Will Play First"
+	else
+		echo "Player2 Will Play First"
+	fi
+}
+
+wantToPlay()
+{
+	read -p "Would You Like To Toss Y/N ?" isToss
+	if [[ $isToss == Y || $isToss == y ]]; 
+	then
+		toss
+	else
+		echo "Okay..Starting Game"
+	fi
+}
+
 initializeBoard
 displayBoard
 letterAssign
+wantToPlay
